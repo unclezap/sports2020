4aee1aaed2d22d396970af16c5cd3b789b17d8fd
unclezap <zeb.blackwell@gmail.com>
2020-04-28 13:43:14 -0700

    this should work

--- to-do.rb
+++
@@ -1,5 +0,0 @@
-# reset CORS stuff in gemfile and in config/initializers/cors
-# change API_ROOT
-#remove oldapp.js and apiscrape.js
-#change api root in redux actions
-#reme week_result_id from week_result
