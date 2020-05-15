52b4fccc95e64a9554706681c1a29af5e1dd853d
unclezap <zeb.blackwell@gmail.com>
2020-05-15 15:45:08 -0700

    restricting cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://sports2020.surge.sh/'
 
     resource '*',
       headers: :any,
